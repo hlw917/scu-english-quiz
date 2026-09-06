f82749d24312fb000cd883fa67b5e9b8abb2ba42
hlw917 <hlw63917@gmail.com>
2026-09-06 20:32:24 +0800

    V1.3.2 修正瀏覽器快取更新

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,47 @@
-const CACHE='scu-english-quiz-v1.3.1';self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png']))) });self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))})
+const CACHE='scu-english-quiz-v1.3.2';
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
+    './','./index.html','./manifest.webmanifest',
+    './icons/icon-192.png','./icons/icon-512.png'
+  ])));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('message',e=>{
+  if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting();
+});
+
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(
+      fetch(e.request,{cache:'no-store'})
+        .then(r=>{
+          const copy=r.clone();
+          caches.open(CACHE).then(c=>c.put('./index.html',copy));
+          return r;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  e.respondWith(
+    fetch(e.request)
+      .then(r=>{
+        if(r&&r.ok){
+          const copy=r.clone();
+          caches.open(CACHE).then(c=>c.put(e.request,copy));
+        }
+        return r;
+      })
+      .catch(()=>caches.match(e.request))
+  );
+});
